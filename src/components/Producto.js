5a98747df0844cd5e0f38915d1c5a6e23f26c17b
blarteagar <arteagarblancal@gmail.com>
2022-10-20 12:21:52 -0500

    deleted some files

--- src/components/Producto.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Product() {
-  return (
-    <div className='product-container'>
-      <img className='product-image' src={require('../images/floating-girl.jpg')} alt='girl floating in a donut floater'/>
-    </div>
-  );
-}

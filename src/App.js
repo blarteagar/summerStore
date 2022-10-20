330089d6db6ba1f43b56208e03433915f2a24c02
blarteagar <arteagarblancal@gmail.com>
2022-10-20 16:00:00 -0500

    estilos de app y producto

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import Product from './components/Product.js';
 function App() {
   return (
     <div className='App'>
-    <div className='main-container'>
-      <h1>SummerStore</h1>
-      <Product />
+      <div className='main-container'>
+        <h1>SummerStore</h1>
+        <Product />
       </div>
     </div>
   );
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import '../stylesheets/Product.css';
 
 function Product() {
   return (
     <div className='product-container'>
-      <img className='product-image' src={require('../images/girl-floating.jpg')} width='50%' alt='girl floating in a donut floater' />
+      <img className='product-image' src={require('../images/girl-floating.jpg')} alt='girl floating in a donut floater' />
       <div className='product-text-container'>
         <p className='product-name'>Floaters</p>
         <p className='product-description'>Hours and hours having fun with the sun</p>

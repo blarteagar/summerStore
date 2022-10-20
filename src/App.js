ef9ef091ed02ad9e972259a53e740d8a000fe7af
blarteagar <arteagarblancal@gmail.com>
2022-10-20 15:24:31 -0500

    some initial styles

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import Product from './components/Product.js';
 function App() {
   return (
     <div className='App'>
-    <h1>SummerStore</h1>
-      <Product/>
+    <div className='main-container'>
+      <h1>SummerStore</h1>
+      <Product />
+      </div>
     </div>
   );
 }
